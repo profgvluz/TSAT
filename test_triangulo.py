b4067f782d47b4548cac425d8024eb585a7e0c7d
GUSTAVO LUZ <gustavo.luz@unisoacad.uniso.br>
2025-02-28 22:25:51 -0300

    pytest github action v13

--- test_triangulo.py
+++ test_triangulo.py
@@ -5,8 +5,7 @@ import pytest
         (1, 2, 4, 5, 7, 1, 10.5), 
         (0, 0, 0, 0, 0, 0, 0),  
         (1, 2, 1, 5, 1, 1, 0),  
-        (1, 2, 4, 6, 7, 8, 3),
-        (1, 2, 4, 6, 7, 8, 4),
+        (1, 2, 4, 6, 7, 8, 3)
     ],)  
 def test_triangulo (a, b, c, d, e,f, expected):    
     assert calcular_area_triangulo(a, b, c, d, e, f) == expected 

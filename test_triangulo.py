ad550e2c762603124595535e8a1e2356969cc802
GUSTAVO LUZ <gustavo.luz@unisoacad.uniso.br>
2025-02-28 21:08:30 -0300

    first commit

---
+++ test_triangulo.py
@@ -0,0 +1,11 @@
+from triangulo import calcular_area_triangulo
+import pytest
+@pytest.mark.parametrize( "a, b, c, d, e,f, expected",  
+    [  
+        (1, 2, 4, 5, 7, 1, 10.5), 
+        (0, 0, 0, 0, 0, 0, 0),  
+        (1, 2, 1, 5, 1, 1, 0),  
+        (1, 2, 4, 6, 7, 8, 3),
+    ],)  
+def test_triangulo (a, b, c, d, e,f, expected):    
+    assert calcular_area_triangulo(a, b, c, d, e, f) == expected 
